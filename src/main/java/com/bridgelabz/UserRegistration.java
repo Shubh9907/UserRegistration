e809c754473e3a3599901bb1f8f56bf7e5f3a213
Shubh9907 <shubhamv60047@gmail.com>
2021-11-08 20:51:22 +0530

    Added code to take valid email id of the user

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -38,10 +38,26 @@ public class UserRegistration {
             lastName(); // Recursively calling lastName() if the user input is invalid
         }
     }
+    /*
+    Method to validate user's email id
+     */
+    public static void validEmail(){
+        System.out.println("Enter Email Id");
+        String emailId = sc.next();
+        String regex = "^[a-zA-Z0-9]+([_+-.][a-zA-Z0-9]+)?[@][a-z]{2,}[.][a-z]{2,4}([.][a-z]{2})?$";
+        boolean b = Pattern.matches(regex, emailId); // if matches then return true
+        if (b==true){
+            System.out.println("Valid");
+        }else {
+            System.out.println("Your input is invalid");
+            validEmail(); // Recursively calling validEmail() if the user input is invalid
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Program");
-        firstName();
-        lastName();
+//        firstName();
+//        lastName();
+        validEmail();
     }
 }
