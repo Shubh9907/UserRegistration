4a664dd156552635556d0d144d4a6c617f59c2cd
Shubh9907 <shubhamv60047@gmail.com>
2021-11-08 19:44:32 +0530

    Added code to take users valid first name

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -1,7 +1,28 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void firstName(){
+        System.out.println("Enter first name");
+        String firstName = sc.next();
+        String regex="^[a-zA-Z]{2,}$";
+        Pattern pattern=Pattern.compile(regex);
+        Matcher matcher=pattern.matcher(firstName);
+        if (matcher.matches()){
+            System.out.println("Valid");
+        }else {
+            System.out.println("Your input is Invalid");
+            firstName();
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Program");
+        firstName();
     }
 }
