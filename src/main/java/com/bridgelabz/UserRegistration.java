95e9f0b2bc0de54fad6a19a025254bb71df89f63
Shubh9907 <shubhamv60047@gmail.com>
2021-11-08 20:01:58 +0530

    Added code to take users valid last name

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -7,6 +7,9 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     static Scanner sc = new Scanner(System.in);
 
+    /*
+    Method to get user's first name
+     */
     public static void firstName(){
         System.out.println("Enter first name");
         String firstName = sc.next();
@@ -17,12 +20,28 @@ public class UserRegistration {
             System.out.println("Valid");
         }else {
             System.out.println("Your input is Invalid");
-            firstName();
+            firstName(); // Recursively calling firstName() if the user input is invalid
+        }
+    }
+    /*
+    Method to get user's last name
+     */
+    public static void lastName(){
+        System.out.println("Enter last name");
+        String lastName = sc.next();
+        String regex="^[a-zA-Z]{2,}$";
+        boolean b = Pattern.matches(regex,lastName); // If these strings matches it returns true
+        if (b==true){
+            System.out.println("Valid");
+        }else {
+            System.out.println("Your input is invalid");
+            lastName(); // Recursively calling lastName() if the user input is invalid
         }
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Program");
         firstName();
+        lastName();
     }
 }
